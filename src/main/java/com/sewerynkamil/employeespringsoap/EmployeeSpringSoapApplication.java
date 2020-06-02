e3e1c6e6a7d21d7ec09a49a095f384d75dd8e924
Kamil Seweryn <kamil_seweryn@o2.pl>
2020-06-02 21:52:15 +0200

    init commit

---
+++ src/main/java/com/sewerynkamil/employeespringsoap/EmployeeSpringSoapApplication.java
@@ -0,0 +1,13 @@
+package com.sewerynkamil.employeespringsoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSpringSoapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeSpringSoapApplication.class, args);
+    }
+
+}
