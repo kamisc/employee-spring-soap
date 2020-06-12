4fcc12ac527ca557564fd9561140d77c40868fea
Kamil Seweryn <kamil_seweryn@o2.pl>
2020-06-12 21:29:58 +0200

    Generating Java code from xml schema using jaxb

---
+++ src/main/java/employee/AddEmployeeResponse.java
@@ -0,0 +1,92 @@
+
+package employee;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="serviceStatus" type="{http://sewerynkamil.pl/employee}serviceStatus"/>
+ *         &lt;element name="employee" type="{http://sewerynkamil.pl/employee}employee"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "serviceStatus",
+    "employee"
+})
+@XmlRootElement(name = "addEmployeeResponse", namespace = "http://sewerynkamil.pl/employee")
+public class AddEmployeeResponse {
+
+    @XmlElement(namespace = "http://sewerynkamil.pl/employee", required = true)
+    protected ServiceStatus serviceStatus;
+    @XmlElement(namespace = "http://sewerynkamil.pl/employee", required = true)
+    protected Employee employee;
+
+    /**
+     * Gets the value of the serviceStatus property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link ServiceStatus }
+     *     
+     */
+    public ServiceStatus getServiceStatus() {
+        return serviceStatus;
+    }
+
+    /**
+     * Sets the value of the serviceStatus property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link ServiceStatus }
+     *     
+     */
+    public void setServiceStatus(ServiceStatus value) {
+        this.serviceStatus = value;
+    }
+
+    /**
+     * Gets the value of the employee property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Employee }
+     *     
+     */
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    /**
+     * Sets the value of the employee property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Employee }
+     *     
+     */
+    public void setEmployee(Employee value) {
+        this.employee = value;
+    }
+
+}
