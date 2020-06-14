e3dc0b8a371c6d7c96401e0c14c36a6fa25e2467
Kamil Seweryn <kamil_seweryn@o2.pl>
2020-06-14 20:36:40 +0200

    Add EmployeeService class with crud operation

---
+++ src/main/java/com/sewerynkamil/employeespringsoap/service/EmployeeService.java
@@ -0,0 +1,59 @@
+package com.sewerynkamil.employeespringsoap.service;
+
+import com.sewerynkamil.employeespringsoap.domain.Employee;
+import com.sewerynkamil.employeespringsoap.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    public EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public Employee getEmployeeById(Long id) {
+        return employeeRepository.findById(id).get();
+    }
+
+    public Employee getEmployeeBySurname(String surname) {
+        return employeeRepository.findBySurname(surname);
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee addEmployee(Employee employee) {
+        try {
+            return employeeRepository.save(employee);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public boolean updateEmployee(Employee employee) {
+        try {
+            employeeRepository.save(employee);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean deleteEmployeeById(Long id) {
+        try {
+            employeeRepository.deleteById(id);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
