2c44f9748a616cb0d18d7e78ec69db689ed2a8d2
Kamil Seweryn <kamil_seweryn@o2.pl>
2020-06-17 21:18:58 +0200

    Adding EmployeeService class

---
+++ src/main/java/com/sewerynkamil/employeespringsoap/service/EmployeeFileService.java
@@ -0,0 +1,49 @@
+package com.sewerynkamil.employeespringsoap.service;
+
+import com.sewerynkamil.employeespringsoap.domain.EmployeeFile;
+import com.sewerynkamil.employeespringsoap.repository.EmployeeFileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeFileService {
+
+    private EmployeeFileRepository employeeFileRepository;
+
+    @Autowired
+    public EmployeeFileService(EmployeeFileRepository employeeFileRepository) {
+        this.employeeFileRepository = employeeFileRepository;
+    }
+
+    public EmployeeFile getEmployeeById(Long id) {
+        return employeeFileRepository.findById(id).get();
+    }
+
+    public EmployeeFile addEmployeeFile(EmployeeFile employeeFile) {
+        try {
+            return employeeFileRepository.save(employeeFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public boolean updateEmployee(EmployeeFile employeeFile) {
+        try {
+            employeeFileRepository.save(employeeFile);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean deleteEmployeeFileById(Long id) {
+        try {
+            employeeFileRepository.deleteById(id);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
