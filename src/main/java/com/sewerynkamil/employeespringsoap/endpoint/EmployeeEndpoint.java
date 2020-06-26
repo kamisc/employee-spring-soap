7d5d5aebec0d4c6354f17a39955c384944be6115
Kamil Seweryn <kamil_seweryn@o2.pl>
2020-06-26 20:34:55 +0200

    Updating EmployeeEndpoint - adding EmployeeMapper functionality to methods

--- src/main/java/com/sewerynkamil/employeespringsoap/endpoint/EmployeeEndpoint.java
+++ src/main/java/com/sewerynkamil/employeespringsoap/endpoint/EmployeeEndpoint.java
@@ -1,17 +1,14 @@
 package com.sewerynkamil.employeespringsoap.endpoint;
 
 import com.sewerynkamil.employeespringsoap.domain.Employee;
+import com.sewerynkamil.employeespringsoap.mapper.EmployeeMapper;
 import com.sewerynkamil.employeespringsoap.service.EmployeeService;
 import com.sewerynkamil.employeespringsoap.req_res.employee.*;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Endpoint
 public class EmployeeEndpoint {
@@ -19,20 +16,21 @@ public class EmployeeEndpoint {
     public static final String NAMESPACE_URI_EMPLOYEE = "http://sewerynkamil.pl/employee";
 
     private EmployeeService employeeService;
+    private EmployeeMapper employeeMapper;
 
     @Autowired
-    public EmployeeEndpoint(EmployeeService employeeService) {
+    public EmployeeEndpoint(EmployeeService employeeService, EmployeeMapper employeeMapper) {
         this.employeeService = employeeService;
+        this.employeeMapper = employeeMapper;
     }
 
     @PayloadRoot(namespace = NAMESPACE_URI_EMPLOYEE, localPart = "getEmployeeByIdRequest")
     @ResponsePayload
     public GetEmployeeByIdResponse getEmployeeById(@RequestPayload GetEmployeeByIdRequest request) {
         GetEmployeeByIdResponse response = new GetEmployeeByIdResponse();
-        Employee employee = employeeService.getEmployeeById(request.getId());
-        EmployeeType employeeType = new EmployeeType();
-        BeanUtils.copyProperties(employee, employeeType);
-        response.setEmployeeType(employeeType);
+        response.setEmployeeType(
+                employeeMapper.mapToEmployeeType(
+                        employeeService.getEmployeeById(request.getId())));
         return response;
     }
 
@@ -40,15 +38,9 @@ public class EmployeeEndpoint {
     @ResponsePayload
     public GetAllEmployeesResponse getAllEmployees(@RequestPayload GetAllEmployeesRequest request) {
         GetAllEmployeesResponse response = new GetAllEmployeesResponse();
-        List<EmployeeType> employeeTypeList = new ArrayList<>();
-        List<Employee> employeeList = employeeService.getAllEmployees();
-        for (Employee employee : employeeList) {
-            EmployeeType employeeType = new EmployeeType();
-            BeanUtils.copyProperties(employee, employeeType);
-            employeeTypeList.add(employeeType);
-        }
-        response.getEmployeeType().addAll(employeeTypeList);
-
+        response.getEmployeeType().addAll(
+                employeeMapper.mapToEmployeeTypeList(
+                        employeeService.getAllEmployees()));
         return response;
     }
 
@@ -56,22 +48,18 @@ public class EmployeeEndpoint {
     @ResponsePayload
     public AddEmployeeResponse addEmployee(@RequestPayload AddEmployeeRequest request) {
         AddEmployeeResponse response = new AddEmployeeResponse();
-        EmployeeType newEmployeeType = new EmployeeType();
         ServiceStatus serviceStatus = new ServiceStatus();
-
-        Employee newEmployee = new Employee(request.getName(), request.getSurname());
-        Employee savedEmployee = employeeService.addEmployee(newEmployee);
+        Employee savedEmployee = employeeService.addEmployee(new Employee(request.getName(), request.getSurname()));
 
         if (savedEmployee == null) {
             serviceStatus.setStatusCode("CONFLICT");
             serviceStatus.setMessage("Exception while adding new Employee");
         } else {
-            BeanUtils.copyProperties(savedEmployee, newEmployeeType);
             serviceStatus.setStatusCode("SUCCESS");
             serviceStatus.setMessage("Employee added successfully");
         }
 
-        response.setEmployeeType(newEmployeeType);
+        response.setEmployeeType(employeeMapper.mapToEmployeeType(savedEmployee));
         response.setServiceStatus(serviceStatus);
         return response;
     }
