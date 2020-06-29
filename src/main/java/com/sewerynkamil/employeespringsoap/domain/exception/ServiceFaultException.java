31738bc6b4ad150d4c2bb76e82213004253e46b6
Kamil Seweryn <kamil_seweryn@o2.pl>
2020-06-29 20:27:40 +0200

    Adding ServiceFaultException class

---
+++ src/main/java/com/sewerynkamil/employeespringsoap/domain/exception/ServiceFaultException.java
@@ -0,0 +1,26 @@
+package com.sewerynkamil.employeespringsoap.domain.exception;
+
+import com.sewerynkamil.employeespringsoap.req_res.employee.ServiceStatus;
+
+public class ServiceFaultException extends RuntimeException {
+
+    private ServiceStatus serviceStatus;
+
+    public ServiceFaultException(String message, ServiceStatus serviceStatus) {
+        super(message);
+        this.serviceStatus = serviceStatus;
+    }
+
+    public ServiceFaultException(String message, Throwable e, ServiceStatus serviceStatus) {
+        super(message, e);
+        this.serviceStatus = serviceStatus;
+    }
+
+    public ServiceStatus getServiceStatus() {
+        return serviceStatus;
+    }
+
+    public void setServiceStatus(ServiceStatus serviceStatus) {
+        this.serviceStatus = serviceStatus;
+    }
+}
